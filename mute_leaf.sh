02f149587cd48fc3752e7709f0e7fc7b2dafa74b
Alex Lance <code@alexlance.com>
2016-10-02 17:03:44 +1100

    Add mute for leaf

--- mute_leaf.sh
+++ mute_leaf.sh
@@ -2,6 +2,8 @@
 
 if amixer sget Master | grep '\[off\]'; then
   amixer sset Master unmute > /dev/null
+  ratpoison -c "echo mute off"
 else
   amixer sset Master mute > /dev/null
+  ratpoison -c "echo mute on"
 fi
