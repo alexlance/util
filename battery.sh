0a3c3981a0f4260744d5265cb2c2d7d4adb8bb91
Alex Lance <code@alexlance.com>
2016-11-01 11:58:05 +1100

    Just a second too late

--- battery.sh
+++ battery.sh
@@ -7,4 +7,4 @@ echo $b
 n="$(acpi -b | grep -i discharging | grep -oE '[0-9]+%' | grep -oE '[0-9]+')"
 
 [ "${n}" ] && [ "${n}" -lt 7 ] && ratpoison -c "echo battery ${b}"
-[ "${n}" ] && [ "${n}" -lt 3 ] && mplayer /home/alla/bin/battery.mp3
+[ "${n}" ] && [ "${n}" -lt 4 ] && mplayer /home/alla/bin/battery.mp3
