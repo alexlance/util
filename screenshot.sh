e9d8e3a487848a30ab5843d66b4c6dda2571dcbc
Alex Lance <code@alexlance.com>
2018-05-15 23:49:59 +1000

    Change screenshot.sh dest path

--- screenshot.sh
+++ screenshot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-(cd /home/alla/scrot && scrot '%Y-%m-%d_%H:%M:%S.png')
-f=/home/alla/scrot/$(ls /home/alla/scrot/ -t | head -1)
+
+p="/home/alla/Download/screenshot/"
+scrot "${p}%Y-%m-%d_%H:%M:%S.png"
+f=${p}$(ls ${p} -t | head -1)
 ratpoison -c "echo ${f}"
 echo $f | xclip -i -selection clipboard -f | xclip -i -selection primary
 
