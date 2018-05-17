626438880d50de9ce0d080cd0c4e7dc060f4693e
Alex Lance <code@alexlance.com>
2018-05-17 17:10:52 +1000

    Mute for sansa

--- mute.sh
+++ mute.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #curr=$(amixer sget 'IEC958 Playback Source' | grep Item0 | cut -d "'" -f2)
-curr=$(amixer sget 'IEC958' | grep 'Mono: Playback \[on\]')
+curr=$(amixer sget 'Headphone' | grep '\[on\]')
 
 if [[ -z "$curr" ]]; then
-  amixer sset "IEC958" on  > /dev/null
+  amixer sset "Headphone" on  > /dev/null
   ratpoison -c "echo mute off"
 else
-  amixer sset "IEC958" off  > /dev/null
+  amixer sset "Headphone" off  > /dev/null
   ratpoison -c "echo mute on"
 fi
 
