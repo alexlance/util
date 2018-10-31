cb8fb8d923cf9ffa5fe8b74a89abd497861e5a9d
Alex Lance <code@alexlance.com>
2018-10-31 22:05:29 +1100

    Make checkout.sh display branch changes

--- checkout.sh
+++ checkout.sh
@@ -6,5 +6,5 @@ git checkout ${1}
 if [ $? != 0 ]; then
   git fetch origin
   git checkout -t -b ${1} origin/${1}
-  git diff --name-only ${1} $(git merge-base FETCH_HEAD master)
+  git diff --summary ${1}..master
 fi
