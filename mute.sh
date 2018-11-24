b57bb260bba591357766ce440a1d1398b9e50a17
Alex Lance <code@alexlance.com>
2018-11-24 11:41:43 +1100

    Fix mute.sh for mint

--- mute.sh
+++ mute.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 #curr=$(amixer sget 'IEC958 Playback Source' | grep Item0 | cut -d "'" -f2)
-curr=$(amixer sget 'IEC958' | grep 'Mono: Playback \[on\]')
+#curr=$(amixer sget 'IEC958' | grep 'Mono: Playback \[on\]')
+curr=$(amixer sget 'Master' | grep '\[on\]')
 
 if [[ -z "$curr" ]]; then
-  amixer sset "IEC958" on  > /dev/null
+  amixer sset "Master" on  > /dev/null
   ratpoison -c "echo mute off"
 else
-  amixer sset "IEC958" off  > /dev/null
+  amixer sset "Master" off  > /dev/null
   ratpoison -c "echo mute on"
 fi
 
