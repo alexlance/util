760312a0c030e45f5bb01e2a0cb69f3737a561be
Alex Lance <code@alexlance.com>
2018-12-15 11:39:57 +1100

    Use number for volume, works on leaf

--- volume.sh
+++ volume.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$1" = "up" ]; then
-  amixer set Master 2dB+ > /dev/null
+  amixer set Master 3000+ > /dev/null
 elif [ "$1" = "down" ]; then
-  amixer set Master 2dB- > /dev/null
+  amixer set Master 3000- > /dev/null
 fi
 
