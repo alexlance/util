3bf3cb90ede12c67ae7af4558d36cfd51e375a8b
Alex Lance <code@alexlance.com>
2019-01-21 10:27:11 +1100

    Add support for bluetooth speaker

---
+++ blue.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euxo pipefail
+
+# bt-adapter -d to show devices
+#echo "trust $mac" | bluetoothctl
+#echo "pair $mac" | bluetoothctl
+
+# this works: pactl set-sink-volume bluez_sink.2C_41_A1_15_2F_8B.a2dp_sink +30%
+
+cat << EOF > /etc/asound.conf
+  defaults.pcm.card 1
+  defaults.pcm.device 2  # the bluetooth speaker number as displayed by `pactl list cards short`
+  defaults.ctl.card 1
+EOF
+
+mac="2C:41:A1:15:2F:8B"
+echo "connect $mac" | bluetoothctl
--- volume.sh
+++ volume.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-if [ "$1" = "up" ]; then
-  amixer set Master 3000+ > /dev/null
-elif [ "$1" = "down" ]; then
-  amixer set Master 3000- > /dev/null
+if [ "$(hostname)" = "leaf" ]; then
+  if [ "$1" = "up" ]; then
+    if pactl list short sinks | grep bluez; then
+      # little orange bose speaker
+      pactl set-sink-volume bluez_sink.2C_41_A1_15_2F_8B.a2dp_sink +10%
+    else
+      amixer set Master 3000+ > /dev/null
+    fi
+  elif [ "$1" = "down" ]; then
+    if pactl list short sinks | grep bluez; then
+      pactl set-sink-volume bluez_sink.2C_41_A1_15_2F_8B.a2dp_sink -10%
+    else
+      amixer set Master 3000- > /dev/null
+    fi
+  fi
+else
+  if [ "$1" = "up" ]; then
+    amixer set Master 3000+ > /dev/null
+  elif [ "$1" = "down" ]; then
+    amixer set Master 3000- > /dev/null
+  fi
 fi
-
