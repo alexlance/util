eac59f04732f2f243c1cb8408994b262caf623a6
Alex Lance <code@alexlance.com>
2020-07-19 10:36:08 +1000

    Use flameshot for screenshot

--- screenshot.sh
+++ screenshot.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-
-p="/home/alla/Download/screenshot/"
-scrot "${p}%Y-%m-%d_%H:%M:%S.png"
-f=${p}$(ls ${p} -t | head -1)
-ratpoison -c "echo ${f}"
-echo $f | xclip -i -selection clipboard -f | xclip -i -selection primary
-
-if which gimp; then
-  gimp $f
-fi
+flameshot gui -p /home/alla/Download/screenshot/
