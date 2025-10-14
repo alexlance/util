a70b02a6b3bdbc6e4bae97c58dbc4de490ff1369
Alex Lance <code@alexlance.com>
2025-10-14 21:49:37 +1100

    Update volums.sh for pea

--- volume.sh
+++ volume.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+#if [ "$1" = "up" ]; then
+#  pulsemixer --change-volume +10
+#  ratpoison -c "echo $(pulsemixer --get-volume)"
+#else
+#  pulsemixer --change-volume -10
+#  ratpoison -c "echo $(pulsemixer --get-volume)"
+#fi
+
 if [ "$1" = "up" ]; then
-  pulsemixer --change-volume +10
+  pulsemixer --change-volume +7
   ratpoison -c "echo $(pulsemixer --get-volume)"
 else
-  pulsemixer --change-volume -10
+  pulsemixer --change-volume -7
   ratpoison -c "echo $(pulsemixer --get-volume)"
 fi
+
+if [ "$(pulsemixer --get-mute)" != 0 ]; then
+  ratpoison -c "echo mute off: $(pulsemixer --get-volume)"
+  pulsemixer --toggle-mute
+fi
+
