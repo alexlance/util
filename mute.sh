a6fe54fef10e0d5ec9bb54a6dee9549aac4fa86e
Alex Lance <code@alexlance.com>
2025-10-14 21:45:16 +1100

    Update mute.sh for pea

--- mute.sh
+++ mute.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+DEFAULT_SINK=$(pactl info | grep "Default Sink" | cut -d " " -f3)
 if [ "$(pulsemixer --get-mute)" = 0 ]; then
   ratpoison -c "echo mute on"
-  pulsemixer --toggle-mute
+  #pulsemixer --toggle-mute
+  pactl set-sink-mute "$DEFAULT_SINK" "1"
 else
   ratpoison -c "echo mute off: $(pulsemixer --get-volume)"
-  pulsemixer --toggle-mute
+  #pulsemixer --toggle-mute
+  pactl set-sink-mute "$DEFAULT_SINK" "0"
 fi
