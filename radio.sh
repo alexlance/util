28f9cc61ee0bafad84e9b6730c84699b621b1a48
Alex Lance <code@alexlance.com>
2025-12-01 10:31:58 +1100

    Adjust ttmux and radio

--- radio.sh
+++ radio.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if pgrep -f triplej; then
-  pkill -f triplej
+if pgrep -f mediaserviceslive; then
+  pkill -f mediaserviceslive
 else
-  mplayer -quiet http://www.abc.net.au/res/streaming/audio/mp3/triplej.pls
+  #mplayer -quiet http://www.abc.net.au/res/streaming/audio/mp3/triplej.pls
+  #mplayer -nocache -quiet http://live-radio01.mediahubaustralia.com/2TJW/mp3/
+  # double J
+  ffplay -loglevel -8 -i https://mediaserviceslive.akamaized.net/hls/live/2038315/doublejnsw/index.m3u8 &
 fi
